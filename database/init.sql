3af67a0b055b125cbbf44bf9a12e8c2ff547d4c6
Paulo Félix <paulofellix@gmail.com>
2023-03-27 17:04:11 -0400

    fix: database init script

--- database/init.sql
+++ database/init.sql
@@ -1,43 +1,43 @@
-SELECT 'CREATE DATABASE pokecenter'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'pokecenter')\gexec
-
 CREATE TABLE CLIENT (
-  id SERIAL PRIMARY KEY,
+  id int PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   phone VARCHAR(255) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 
-CREATE ENUM TRANSACTION_TYPE AS ENUM ('CREDIT', 'DEBIT');
-CREATE TABLE TRANSACTIONS (
+CREATE TYPE TRANSACTION_TYPE AS ENUM ('CREDIT', 'DEBIT');
+CREATE TABLE TRANSACTION (
   id SERIAL PRIMARY KEY,
+  client_id int not null,
   amount DECIMAL(10,2) NOT NULL,
   type TRANSACTION_TYPE NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  FOREIGN KEY (user_id) REFERENCES CLIENT(id)
+  FOREIGN KEY (client_id) REFERENCES CLIENT(id)
 );
 
 CREATE TABLE BALANCE (
   id SERIAL PRIMARY KEY,
+  client_id int not null,
   amount DECIMAL(10,2) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  FOREIGN KEY (user_id) REFERENCES CLIENT(id)
+  FOREIGN KEY (client_id) REFERENCES CLIENT(id)
 );
 
-INSERT INTO CLIENT (name, phone) VALUES ('John Doe', '123456789');
-INSERT INTO CLIENT (name, phone) VALUES ('Jane Doe', '987654321');
-INSERT INTO CLIENT (name, phone) VALUES ('John Smith', '123456789');
-INSERT INTO CLIENT (name, phone) VALUES ('Jane Smith', '987654321');
-INSERT INTO TRANSACTIONS (amount, type, user_id) VALUES (100.00, 'CREDIT', 1);
-INSERT INTO TRANSACTIONS (amount, type, user_id) VALUES (50.00, 'DEBIT', 1);
-INSERT INTO TRANSACTIONS (amount, type, user_id) VALUES (100.00, 'CREDIT', 2);
-INSERT INTO TRANSACTIONS (amount, type, user_id) VALUES (50.00, 'DEBIT', 2);
-INSERT INTO TRANSACTIONS (amount, type, user_id) VALUES (100.00, 'CREDIT', 3);
-INSERT INTO TRANSACTIONS (amount, type, user_id) VALUES (50.00, 'DEBIT', 3);
-INSERT INTO TRANSACTIONS (amount, type, user_id) VALUES (100.00, 'CREDIT', 4);
-INSERT INTO TRANSACTIONS (amount, type, user_id) VALUES (50.00, 'DEBIT', 4);
-INSERT INTO BALANCE (amount, user_id) VALUES (50.00, 1);
-INSERT INTO BALANCE (amount, user_id) VALUES (50.00, 2);
-INSERT INTO BALANCE (amount, user_id) VALUES (50.00, 3);
-INSERT INTO BALANCE (amount, user_id) VALUES (50.00, 4);
+
+INSERT INTO CLIENT (id, name, phone) VALUES (1, 'John Doe', '123456789');
+INSERT INTO CLIENT (id, name, phone) VALUES (2, 'Jane Doe', '987654321');
+INSERT INTO CLIENT (id, name, phone) VALUES (3, 'John Smith', '123456789');
+INSERT INTO CLIENT (id, name, phone) VALUES (4, 'Jane Smith', '987654321');
+INSERT INTO "transaction" (amount, type, client_id) VALUES (100.00, 'CREDIT', 1);
+INSERT INTO "transaction" (amount, type, client_id) VALUES (50.00, 'DEBIT', 1);
+INSERT INTO "transaction" (amount, type, client_id) VALUES (100.00, 'CREDIT', 2);
+INSERT INTO "transaction" (amount, type, client_id) VALUES (50.00, 'DEBIT', 2);
+INSERT INTO "transaction" (amount, type, client_id) VALUES (100.00, 'CREDIT', 3);
+INSERT INTO "transaction" (amount, type, client_id) VALUES (50.00, 'DEBIT', 3);
+INSERT INTO "transaction" (amount, type, client_id) VALUES (100.00, 'CREDIT', 4);
+INSERT INTO "transaction" (amount, type, client_id) VALUES (50.00, 'DEBIT', 4);
+INSERT INTO BALANCE (amount, client_id) VALUES (50.00, 1);
+INSERT INTO BALANCE (amount, client_id) VALUES (50.00, 2);
+INSERT INTO BALANCE (amount, client_id) VALUES (50.00, 3);
+INSERT INTO BALANCE (amount, client_id) VALUES (50.00, 4);
